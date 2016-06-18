5842a766ea056f98807d2ea5d0a9205d11e1ed12
Vishal Parashar <vishalparashar496@gmail.com>
2016-06-18 13:12:37 +0530

    changed $searchResponse to $viewResponse

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 	@if ($count>0) 
 		@for ($i=0;$i<$count;$i++)  <!--Looping through response array-->
-			<h3><a href="{{ $searchResponse[$type][$i]['url'] }}"> {{ $searchResponse[$type][$i]['title'] }} </a></h3>
-			<p>{{ $searchResponse[$type][$i]['kwic'] }}</p>
+			<h3><a href="{{ $viewResponse[$type][$i]['url'] }}"> {{ $viewResponse[$type][$i]['title'] }} </a></h3>
+			<p>{{ $viewResponse[$type][$i]['kwic'] }}</p>
 		@endfor
 	@endif	
 @stop
--- resources/views/trending.blade.php
+++ resources/views/trending.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	@if ($count>0) 
 		@for ($i=0;$i<$count;$i++)  <!--Looping through response array-->
-			<h3><a href="{{ $searchResponse[$type][$i]['related'][0]['url'] }}"> {{ $searchResponse[$type][$i]['name'] }} </a></h3>
+			<h3><a href="{{ $viewResponse[$type][$i]['related'][0]['url'] }}"> {{ $viewResponse[$type][$i]['name'] }} </a></h3>
 		@endfor
 	@endif	
 @stop
