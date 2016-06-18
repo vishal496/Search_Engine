04afec0430c6140a2047aa15f23b6ef445ffc172
Vishal Parashar <vishalparashar496@gmail.com>
2016-06-18 12:14:22 +0530

    Given proper function definition and moved the $query from model to controller and combined the $rawUrl and $url to make a single $url

--- app/Result.php
+++ app/Result.php
@@ -7,25 +7,22 @@ use Illuminate\Http\Request;
 
 class Result extends Model
 {
-    protected $response;
 
     function __construct(Request $request)
     {
-    	$this->response = $request;
+    	
     }
 
     /*
-     * This method gets the search result for the query made by the user. 
+     * This method gets the search result for the query made by the user from the faroo search api. 
      *
-     * @param param_type param_name param_description
-     * @param string     src        type of search made by user 
-
-     * @return php array 
+     * @param string     src        type of search made by user. 
+     * @param string     query      query made by user.
+     * @return php array which was converted from a json response received from the faroo search api.
+     *
      */
-    public function getFarooResponse($src)
+    public function getFarooResponse($src,$query)
     {
-    	// Query made by user.
-    	 $query = $this->response->input('query');
  		
         //formating the url using php function.
         $data = array('q'=>$query,
@@ -36,21 +33,19 @@ class Result extends Model
                       'i'=>'false',
                       'f'=>'json');
 
-        $rawUrl = http_build_query($data);
-
         // api key and api url in .env file
         // Url to be hit for the search.
-        $url = env('API_URL').$rawUrl."&key=".env('API_KEY');
-
+        $url = env('API_URL').http_build_query($data)."&key=".env('API_KEY');
+        
 		// Curl request handling.
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$maps_json = curl_exec($ch);
+		$mapsJson = curl_exec($ch);
 		curl_close($ch);
 
 		// Converting the JSON response to a PHP array.
-		$response = json_decode(($maps_json),true);
+		$responseArray = json_decode(($mapsJson),true);
 
-		return $response;
+		return $responseArray;
     }
 }
